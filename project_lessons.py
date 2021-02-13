69938effca871916e5a88a69cb0155b881a02e78
Benjamin Nicholson <Chimera@Vampiricaura66@gmail.com>
2021-02-13 15:13:06 +1000

    practiced loops with dictionaries

---
+++ project_lessons.py
@@ -0,0 +1,9 @@
+# phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+# for key, value in phone_numbers.items():
+#   print("{}: {}".format(key, value))
+
+# phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+# for values in phone_numbers.values():
+#    new_number = values.replace("+", "00")
+#    print(new_number)
+
